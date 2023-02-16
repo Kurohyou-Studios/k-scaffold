6f0ecccd77e61a1349a97cfeab19e6a05fcb7ea5
Automated Continuous Integration <Riernar@users.noreply.github.com>
2023-02-16 02:15:29 +0000

    Continuous Integration - Build Doc

---
+++ docs/assets/Style-51936a6d.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Library-64325cd2.js";import{s as e,o as s,h as o,w as c,u as n,a as r}from"./index-8605ff5c.js";const _=r("p",null,"Placeholder description",-1),i={__name:"Style",setup(m){return e.map(a=>({...a,context:{...a.context,name:`k.${a.context.name}`}})),(a,p)=>(s(),o(t,{data:n(e)},{default:c(()=>[_]),_:1},8,["data"]))}};export{i as default};
